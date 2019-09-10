a2bcdb4cd04a2af6ad0987d52fc0108047083c08
Teah <teahkm@student.matnat.uio.no>
2019-09-10 15:42:00 +0200

    add function for creating design matrix

--- project1/frankefunction_analysis.py
+++ project1/frankefunction_analysis.py
@@ -27,8 +27,29 @@ y_flat = np.ravel(y)
 
 # need to make a function for arbitrary order
 # design matrix for order 2
-X = np.c_[np.ones(400),x_flat,y_flat,x_flat**2, y_flat**2, x_flat*y_flat]
+#X = np.c_[np.ones(400),x_flat,y_flat,x_flat**2, y_flat**2, x_flat*y_flat]
 
+def CreateDesignMatrix_X(x, y, n = 5):
+	"""
+	Function for creating a design X-matrix with rows [1, x, y, x^2, xy, xy^2 , etc.]
+	Input is x and y mesh or raveled mesh, keyword agruments n is the degree of the polynomial you want to fit.
+	"""
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+
+	N = len(x)
+	l = int((n+1)*(n+2)/2)		# Number of elements in beta
+	X = np.ones((N,l))
+
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = x**(i-k) * y**k
+
+	return X
+
+X = CreateDesignMatrix_X(x,y,n=2)
 
 # manual inversion
 beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z_flat)
@@ -59,3 +80,17 @@ for b in range(len(beta)):
     upper = beta[b] + 1.96*np.sqrt(var_b)
     lower = beta[b] - 1.96*np.sqrt(var_b)
     print("Confidence interval: [%g,%g]" %(lower, upper))
+
+
+#copied from the internet, use for inspiration
+def k_fold_cross_validation(dataset, folds=3):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / folds)
+	for i in range(folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
