be8d26eca8ccf5522587331b0c3743abee80ad0f
Teah <teahkm@student.matnat.uio.no>
2019-09-14 13:03:32 +0200

    moved common stuff into common module

--- project1/frankefunction_analysis.py
+++ project1/frankefunction_analysis.py
@@ -2,20 +2,8 @@ import numpy as np
 import sklearn.linear_model as skl
 from sklearn.metrics import mean_squared_error, r2_score
 
-#make data
-x = np.arange(0,1,0.05)
-y = np.arange(0,1,0.05)
-x,y = np.meshgrid(x,y)
 
-#franke function
-def franke_function(x,y):
-    term1 = 3/4*np.exp(-(9*x-2)**2/4 - (9*y-2)**2/4)
-    term2 = 3/4*np.exp(-(9*x+1)**2/49 - (9*y+1)/10)
-    term3 = 1/2*np.exp(-(9*x-7)**2/4-(9*y-3)**2/4)
-    term4 = -1/5*np.exp(-(9*x-4)**2-(9*y-7)**2)
-
-    return term1 + term2 + term3 + term4
-
+from common import franke_function, x, y, CreateDesignMatrix_X
 
 
 z = franke_function(x,y) #+ np.random.randn(20,20)
@@ -25,29 +13,6 @@ z_flat = np.ravel(z)
 x_flat = np.ravel(x)
 y_flat = np.ravel(y)
 
-# need to make a function for arbitrary order
-# design matrix for order 2
-#X = np.c_[np.ones(400),x_flat,y_flat,x_flat**2, y_flat**2, x_flat*y_flat]
-
-def CreateDesignMatrix_X(x, y, n = 5):
-	"""
-	Function for creating a design X-matrix with rows [1, x, y, x^2, xy, xy^2 , etc.]
-	Input is x and y mesh or raveled mesh, keyword agruments n is the degree of the polynomial you want to fit.
-	"""
-	if len(x.shape) > 1:
-		x = np.ravel(x)
-		y = np.ravel(y)
-
-	N = len(x)
-	l = int((n+1)*(n+2)/2)		# Number of elements in beta
-	X = np.ones((N,l))
-
-	for i in range(1,n+1):
-		q = int((i)*(i+1)/2)
-		for k in range(i+1):
-			X[:,q+k] = x**(i-k) * y**k
-
-	return X
 
 X = CreateDesignMatrix_X(x,y,n=2)
 
