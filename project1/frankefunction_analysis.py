76260ec5d9da58243b69c0225d7b7b8963a32e23
Teah <teahkm@student.matnat.uio.no>
2019-09-19 15:38:16 +0200

    changed file to import from common module

--- project1/frankefunction_analysis.py
+++ project1/frankefunction_analysis.py
@@ -4,28 +4,15 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 
 from common import franke_function, x, y, CreateDesignMatrix_X
+from regression import OLS_fit, OLS_predict
 
 #try with noise
 z = franke_function(x,y) #+ np.random.randn(20,20)
 z_flat = np.ravel(z)
 
-
-x_flat = np.ravel(x)
-y_flat = np.ravel(y)
-
-
 X = CreateDesignMatrix_X(x,y,n=2)
 
-# manual inversion
-#beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z_flat)
-
-#def OLS(X, y):
-    #beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-    #ytilde = X.dot(beta)
-
-    #return beta, ytilde
-
-# FIX
+"""
 def OLS_fit(X,y):
     #pseudo inverse, SVD
     A = np.linalg.pinv(X)
@@ -35,33 +22,39 @@ def OLS_fit(X,y):
 
 def OLS_predict(beta, X):
     return X.dot(beta)
+"""
 
-# scikit-learn
-#clf = skl.LinearRegression().fit(X,z_flat)
-#z_tilde = clf.predict(X)
+def main():
+    # scikit-learn
+    #clf = skl.LinearRegression().fit(X,z_flat)
+    #z_tilde = clf.predict(X)
 
-#beta, z_tilde = OLS(X, z_flat)
-beta = OLS_fit(X,z_flat)
-z_tilde = OLS_predict(beta, X)
+    #beta, z_tilde = OLS(X, z_flat)
+    beta = OLS_fit(X,z_flat)
+    z_tilde = OLS_predict(beta, X)
 
-# MSE
-MSE = mean_squared_error(z_flat,z_tilde)
+    # MSE
+    MSE = mean_squared_error(z_flat,z_tilde)
 
-# R²
-R_squared = r2_score(z_flat,z_tilde)
+    # R²
+    R_squared = r2_score(z_flat,z_tilde)
 
-print("MSE: %.2f" %MSE)
-print("R_squared: %.2f" %R_squared)
+    print("MSE: %.2f" %MSE)
+    print("R_squared: %.2f" %R_squared)
 
 
-# variance of betas
-var_beta = np.linalg.inv(X.T.dot(X))
+    # variance of betas
+    var_beta = np.linalg.inv(X.T.dot(X))
 
-# confidence interval for betas
-for b in range(len(beta)):
-    #sigma squared is 1
-    var_b = var_beta[b][b]
-    #print (var_b)
-    upper = beta[b] + 1.96*np.sqrt(var_b)
-    lower = beta[b] - 1.96*np.sqrt(var_b)
-    print("Confidence interval: [%g,%g]" %(lower, upper))
+    # confidence interval for betas
+    for b in range(len(beta)):
+        #sigma squared is 1
+        var_b = var_beta[b][b]
+        #print (var_b)
+        upper = beta[b] + 1.96*np.sqrt(var_b)
+        lower = beta[b] - 1.96*np.sqrt(var_b)
+        print("Confidence interval for beta_%d: [%g,%g]" %(b,lower, upper))
+
+
+if __name__ == "__main__":
+    main()
