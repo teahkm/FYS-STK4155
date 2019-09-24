6a99af03090c5c4f41e4ab19261b59e87d52ba12
Teah <teahkm@student.matnat.uio.no>
2019-09-24 15:42:59 +0200

    updated CV so bias variance sort of works

--- project1/regression.py
+++ project1/regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
 
 def franke_function(x,y):
     """
@@ -115,18 +116,19 @@ def Ridge_predict(beta, X):
 def Lasso_fit(X, y, _lambda=0.1):
     clf = Lasso(alpha=_lambda)
     clf.fit(X, y)
-    beta = np.zeros(len(clf.coef_)+1)
-    beta[0] = clf.intercept_
-    beta[1:] = clf.coef_
-    return beta, #clf
+    #beta = np.zeros(len(clf.coef_)+1)
+    beta = clf.coef_
+    #beta[0] = clf.intercept_
+    #beta[1:] = clf.coef_
+    return beta.T #clf
 
-#def Lasso_predict(X, y, _lambda=0.1):
-#    clf = Lasso(alpha=_lambda)
-#    clf.fit(X,y)
-#    return clf.predict(X)
+def Lasso_predict(beta, X):
+    return X.dot(beta)
 
 
-def k_fold_cross_validation(X, z, k=5, fit_type=OLS_fit, predict_type= OLS_predict, **parameters):
+
+
+def k_fold_cross_validation(X, z, k=5, fit_type=OLS_fit, predict_type= OLS_predict, tradeoff = False, **parameters):
     """ A function that predicts a value using linear regression.
 
         Args:
@@ -142,8 +144,79 @@ def k_fold_cross_validation(X, z, k=5, fit_type=OLS_fit, predict_type= OLS_predi
             tot_bias (float): average bias after k folds
             tot_var (float): average variance of predicted values after k folds
     """
+    MSE_test = []
+    MSE_train = []
+    bias = []
+    variance = []
 
-    MSE = []
+    # shuffling data
+    index = np.random.permutation(np.arange(len(z)))
+    X_shuffled = X[index]
+    z_shuffled = z[index]
+
+    # split into folds
+
+
+    if not tradeoff:
+        i = np.arange(len(z)) % k
+
+        for fold in range(k):
+            X_test = X_shuffled[i==fold]
+            X_train = X_shuffled[i!=fold]
+            z_test = z_shuffled[i==fold]
+            z_train = z_shuffled[i!=fold]
+
+            # fit model
+            beta_train = fit_type(X_train, z_train, **parameters)
+            z_tilde_test = predict_type(beta_train, X_test)
+            z_tilde_train = predict_type(beta_train, X_train)
+
+            # evaluate MSE, bias, variance
+            MSE_test.append(np.mean((z_test - z_tilde_test)**2))
+            MSE_train.append(np.mean((z_train - z_tilde_train)**2))
+            #bias.append(np.mean((z_test - np.mean(z_tilde_test))**2))
+            #variance.append(np.var(z_tilde_test))
+            return MSE_test, MSE_train
+
+    else:
+        X_shuf_train, X_shuf_test, z_shuf_train, z_shuf_test = train_test_split(
+                                                                X_shuffled, z_shuffled, test_size = 0.33)
+
+        z_all_preds = np.empty((z_shuf_test.shape[0], k))
+        #print("all preds: ", z_all_preds.shape)
+
+        i = np.arange(len(z_shuf_train)) % k
+
+        for fold in range(k):
+            X_test = X_shuf_train[i==fold]
+            X_train = X_shuf_train[i!=fold]
+            z_test = z_shuf_train[i==fold]
+            z_train = z_shuf_train[i!=fold]
+
+            # fit model
+            beta_train = fit_type(X_train, z_train, **parameters)
+            z_tilde_test = predict_type(beta_train, X_shuf_test)
+            #print("z tilde test: ", z_tilde_test.shape)
+            #z_tilde_train = predict_type(beta_train, X_train)
+            z_all_preds[:, fold] = z_tilde_test
+
+            # evaluate MSE, bias, variance
+            #MSE_test.append(np.mean((z_test - z_tilde_test)**2))
+            #MSE_train.append(np.mean((z_train - z_tilde_train)**2))
+            #bias.append(np.mean((z_test - np.mean(z_tilde_test))**2))
+            #variance.append(np.var(z_tilde_test))
+
+    # average the results
+        print("z_test: ", z_shuf_test.shape)
+        print("z_pred: ", z_all_preds.shape)
+        tot_err_test = np.mean((z_shuf_test - np.mean(z_all_preds, axis=1))**2 )
+        tot_bias = np.mean( (z_shuf_test - np.mean(z_all_preds, axis=1, keepdims=True))**2 )
+        tot_var = np.mean( np.var(z_all_preds, axis=1) )
+
+        return tot_err_test, tot_bias, tot_var
+
+    """MSE_train = []
+    MSE_test = []
     bias = []
     variance = []
 
@@ -154,24 +227,38 @@ def k_fold_cross_validation(X, z, k=5, fit_type=OLS_fit, predict_type= OLS_predi
 
     # split into folds
     i = np.arange(len(z)) % k
+    print(i.shape)
+    z_pred = np.empty((z[i==1].shape[0], k))
+    z_test_arr = np.empty((z[i==1].shape[0], k))
     for fold in range(k):
         X_test = X_shuffled[i==fold]
         X_train = X_shuffled[i!=fold]
         z_test = z_shuffled[i==fold]
         z_train = z_shuffled[i!=fold]
 
+        z_test_arr[:, fold] = z_test
+
         # fit model
         beta_train = fit_type(X_train, z_train, **parameters)
-        z_tilde = predict_type(beta_train, X_test)
+        z_tilde =predict_type(beta_train, X_test).ravel()
+        print(predict_type(beta_train, X_test).shape)
+        print(z_pred.shape)
+        z_pred[:, fold] = z_tilde
 
         # evaluate MSE, bias, variance
-        MSE.append(np.mean((z_test - z_tilde)**2))
-        bias.append(np.mean((z_test - np.mean(z_tilde))**2))
-        variance.append(np.var(z_tilde))
+        MSE_test.append(np.mean((z_test - z_tilde)**2))
+        MSE_train.append(np.mean((z_train - z_tilde)**2))
+        #bias.append(np.mean((z_test - np.mean(z_tilde))**2))
+        #variance.append(np.var(z_tilde))
 
     # average the results
-    tot_err = np.mean(MSE)
-    tot_bias = np.mean(bias)
-    tot_var = np.mean(variance)
+    #MSE_test = np.mean( np.mean((z_test_arr - z_pred)**2, axis=1) )
+    bias = np.mean( (z_test_arr - np.mean(z_pred, axis=1, keepdims=True))**2 )
+    variance = np.mean( np.var(z_pred, axis=1) )
+    #tot_err = np.mean(MSE)
+    #tot_bias = np.mean(bias)
+    #tot_var = np.mean(variance)
 
-    return tot_err, tot_bias, tot_var
+    return MSE_test, MSE_train, bias, variance
+    #return tot_err, tot_bias, tot_var
+"""
