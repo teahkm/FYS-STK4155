0822d3b95eeab77f720bf965c41506d6a7953761
Teah <teahkm@student.matnat.uio.no>
2019-10-07 14:50:37 +0200

    fixed comments and docstrings

--- project1/src/regression.py
+++ project1/src/regression.py
@@ -1,10 +1,13 @@
+# module for regression methods
+
 import numpy as np
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+
 def franke_function(x,y):
-    """
+    '''
 	Test function.
 
     Args:
@@ -13,7 +16,8 @@ def franke_function(x,y):
 
     Returns:
         f(x,y)
-	"""
+	'''
+
     term1 = 3/4*np.exp(-(9*x-2)**2/4 - (9*y-2)**2/4)
     term2 = 3/4*np.exp(-(9*x+1)**2/49 - (9*y+1)/10)
     term3 = 1/2*np.exp(-(9*x-7)**2/4-(9*y-3)**2/4)
@@ -21,11 +25,14 @@ def franke_function(x,y):
 
     return term1 + term2 + term3 + term4
 
+
+# code written by Morten
 def CreateDesignMatrix_X(x, y, n = 5):
-	"""
+	'''
 	Function for creating a design X-matrix with rows [1, x, y, x^2, xy, xy^2 , etc.]
 	Input is x and y mesh or raveled mesh, keyword agruments n is the degree of the polynomial you want to fit.
-	"""
+	'''
+
 	if len(x.shape) > 1:
 		x = np.ravel(x)
 		y = np.ravel(y)
@@ -41,11 +48,14 @@ def CreateDesignMatrix_X(x, y, n = 5):
 
 	return X
 
+
+# code written by Morten
 def SVDinv(A):
     ''' Takes as input a numpy matrix A and returns inv(A) based on singular value decomposition (SVD).
     SVD is numerically more stable than the inversion algorithms provided by
     numpy and scipy.linalg at the cost of being slower.
     '''
+
     U, s, VT = np.linalg.svd(A)
 
     D = np.zeros((len(U),len(VT)))
@@ -53,6 +63,7 @@ def SVDinv(A):
         D[i,i]=s[i]
     UT = np.transpose(U); V = np.transpose(VT); invD = np.linalg.inv(D)
     return np.matmul(V,np.matmul(invD,UT))
+
 
 def CI_beta(X, beta):
     ''' A function that calculates the upper and lower boundaries of a 95 percent confidence
@@ -85,7 +96,7 @@ def CI_beta(X, beta):
 
 
 def OLS_fit(X,y):
-    """ A function that caluculates the coefficients beta_i in ordinary least squares.
+    ''' A function that caluculates the coefficients beta_i in ordinary least squares.
 
         Args:
             X (matrix): design matrix
@@ -93,16 +104,15 @@ def OLS_fit(X,y):
 
         Returns:
             beta (array): regression coefficients
-    """
-    #pseudo inverse, SVD
-    #A = np.linalg.pinv(X)
+    '''
+
     inv = SVDinv(X.T.dot(X))
-    #beta = A.dot(y)
     beta = inv.dot(X.T).dot(y)
     return beta
 
+
 def OLS_predict(beta, X):
-    """ A function that predicts a value using ordinary least squares.
+    ''' A function that predicts a value using ordinary least squares.
 
         Args:
             beta (array): regression coefficients
@@ -110,11 +120,13 @@ def OLS_predict(beta, X):
 
         Returns:
             predicted value of X times beta
-    """
+    '''
+
     return X.dot(beta)
 
+
 def Ridge_fit(X, y, _lambda=0.1):
-    """ A function that calculates the coefficients beta_i using Ridge regression.
+    ''' A function that calculates the coefficients beta_i using Ridge regression.
 
         Args:
             X (matrix): design matrix
@@ -123,7 +135,7 @@ def Ridge_fit(X, y, _lambda=0.1):
 
         Returns:
             beta (array): regression coefficients
-    """
+    '''
 
     XtX = X.T.dot(X) #help variable
     inv = SVDinv(XtX + _lambda*np.identity(len(XtX[0])))
@@ -131,8 +143,9 @@ def Ridge_fit(X, y, _lambda=0.1):
     beta = inv.dot(X.T).dot(y)
     return beta
 
+
 def Ridge_predict(beta, X):
-    """ A function that predicts a value using Ridge regression.
+    ''' A function that predicts a value using Ridge regression.
 
         Args:
             beta (array): regression coefficients
@@ -140,11 +153,13 @@ def Ridge_predict(beta, X):
 
         Returns:
             predicted value of X times beta
-    """
+    '''
+
     return X.dot(beta)
 
+
 def Lasso_fit(X, y, _lambda=0.1):
-    """ A function that fits a model using Ridge regression.
+    ''' A function that fits a model using Lasso regression.
 
         Args:
             X (matrix): design matrix
@@ -153,7 +168,7 @@ def Lasso_fit(X, y, _lambda=0.1):
 
         Returns:
             model (Lasso object): the model that has been fitted
-    """
+    '''
 
     model = Lasso(alpha=_lambda)
     # does not include the intercept column
@@ -161,8 +176,9 @@ def Lasso_fit(X, y, _lambda=0.1):
 
     return model
 
+
 def Lasso_predict(model, X):
-    """ A function that predicts a value using Lasso regression.
+    ''' A function that predicts a value using Lasso regression.
 
         Args:
             model (Lasso object): a model that has been fitted
@@ -170,16 +186,14 @@ def Lasso_predict(model, X):
 
         Returns:
             predicted values
-    """
+    '''
+
     # does not include intercept column
     return model.predict(X[:,1:])
 
 
-
-
-
 def k_fold_cross_validation(X, z, k=10, fit_type=OLS_fit, predict_type= OLS_predict, tradeoff = False, **parameters):
-    """ A function that predicts a value using linear regression.
+    ''' A function that predicts a value using linear regression.
 
         Args:
             X (matrix): design matrix
@@ -187,13 +201,16 @@ def k_fold_cross_validation(X, z, k=10, fit_type=OLS_fit, predict_type= OLS_pred
             k (int): number of folds
             fit_type (function): which regression fit to use
             predict_type (function): which regression to use for prediction
+            tradeoff (boolean): decided whether or not to calculate bias and variance
 
 
         Returns:
-            tot_err (float): average mean squared error after k folds
+            tot_err_test (float): average MSE from test data after k folds
+            tot_err_train (float): average MSE from train data after k fold
             tot_bias (float): average bias after k folds
             tot_var (float): average variance of predicted values after k folds
-    """
+    '''
+
     MSE_test = []
     MSE_train = []
     bias = []
@@ -220,22 +237,21 @@ def k_fold_cross_validation(X, z, k=10, fit_type=OLS_fit, predict_type= OLS_pred
             z_tilde_test = predict_type(beta_train, X_test)
             z_tilde_train = predict_type(beta_train, X_train)
 
-            # evaluate MSE, bias, variance
+            # evaluate test and train MSE
             MSE_test.append(np.mean((z_test - z_tilde_test)**2))
             MSE_train.append(np.mean((z_train - z_tilde_train)**2))
-            #bias.append(np.mean((z_test - np.mean(z_tilde_test))**2))
-            #variance.append(np.var(z_tilde_test))
 
         tot_err_test = np.mean(MSE_test)
         tot_err_train = np.mean(MSE_train)
         return tot_err_test, tot_err_train
 
+    # if we want the bias-variance tradeoff
+    # this part of the code does not yield the expected results, use bootstrap instead
     else:
         X_shuf_train, X_shuf_test, z_shuf_train, z_shuf_test = train_test_split(
                                                                 X_shuffled, z_shuffled, test_size = 0.33)
 
         z_all_preds = np.empty((z_shuf_test.shape[0], k))
-        #print("all preds: ", z_all_preds.shape)
 
         i = np.arange(len(z_shuf_train)) % k
 
@@ -248,39 +264,44 @@ def k_fold_cross_validation(X, z, k=10, fit_type=OLS_fit, predict_type= OLS_pred
             # fit model
             beta_train = fit_type(X_train, z_train, **parameters)
             z_tilde_test = predict_type(beta_train, X_shuf_test)
-            #print("z tilde test: ", z_tilde_test.shape)
-            #z_tilde_train = predict_type(beta_train, X_train)
             z_all_preds[:, fold] = z_tilde_test
 
-            # evaluate MSE, bias, variance
-            #MSE_test.append(np.mean((z_test - z_tilde_test)**2))
-            #MSE_train.append(np.mean((z_train - z_tilde_train)**2))
-            #bias.append(np.mean((z_test - np.mean(z_tilde_test))**2))
-            #variance.append(np.var(z_tilde_test))
-
-    # average the results
 
         tot_err_test = mean_squared_error(z_shuf_test, np.mean(z_all_preds, axis=1))
-        #tot_err_test = np.mean( np.mean((z_shuf_test - np.mean(z_all_preds))**2, axis=1) )
-        #tot_err_test = np.mean( np.mean((z_shuf_test - np.mean(z_all_preds,axis=1))**2, axis=1) )
-        #tot_err_test = np.mean(np.mean((z_shuf_test - np.mean(z_all_preds, axis=1))**2))
-        #tot_err_test = np.mean( (z_shuf_test - np.mean(z_all_preds, axis=1, keepdims=True))**2 )
         tot_bias = np.mean( (z_shuf_test - np.mean(z_all_preds, axis=1, keepdims=True))**2 )
         tot_var = np.mean( np.var(z_all_preds, axis=1) )
 
         return tot_err_test, tot_bias, tot_var
 
+
+# code inspired by Morten's code
 def bootstrap(X, z, num_bootstraps, fit_type=OLS_fit, predict_type= OLS_predict, **parameters):
+    ''' A function that calculates the MSE, bias and variance using the bootstrap method.
+
+        Args:
+            X (matrix): design matrix
+            z (array): true values
+            num_bootstraps(int or float): number of bootstrap iterations
+            fit_type (function): which regression fit to use
+            predict_type (function): which regression to use for prediction
+
+        Returns:
+            error_test (float): the average test MSE after num_bootstraps iterations
+            error_train (float): the average train MSE after num_bootstraps iterations
+            bias (float): the average bias after num_bootstraps iterations
+            variance (float): the average variance after num_bootstraps iterations
+    '''
+
     from sklearn.utils import resample
 
     X_train, X_test, z_train, z_test = train_test_split(X, z, test_size = 0.33)
-    z_pred_test = np.empty((z_test.shape[0], num_bootstraps)) #collects all the test predictions
-    z_pred_train = np.empty((z_train.shape[0], num_bootstraps)) #collects all the train predictions
+    z_pred_test = np.empty((z_test.shape[0], num_bootstraps)) # collects all the test predictions
+    z_pred_train = np.empty((z_train.shape[0], num_bootstraps)) # collects all the train predictions
 
     for i in range(num_bootstraps):
         X_, z_ = resample(X_train, z_train)
         beta_train = fit_type(X_, z_, **parameters)
-        z_tilde_test = predict_type(beta_train, X_test) #prediction for current iteration
+        z_tilde_test = predict_type(beta_train, X_test) # prediction for current iteration
         z_tilde_train = predict_type(beta_train, X_train)
         z_pred_test[:,i] = z_tilde_test
         z_pred_train[:,i] = z_tilde_train
