434f22a7072148bcbf756a80cf11420fc13bbfb1
Teah <teahkm@student.matnat.uio.no>
2019-10-07 15:00:07 +0200

    added docstrings

--- project1/src/test_regression.py
+++ project1/src/test_regression.py
@@ -4,7 +4,7 @@ import sklearn.linear_model as skl
 from sklearn.metrics import mean_squared_error, r2_score, make_scorer
 from sklearn.model_selection import KFold, cross_validate
 
-
+# make data
 x = np.arange(0,1,0.05)
 y = np.arange(0,1,0.05)
 x,y = np.meshgrid(x,y)
@@ -14,6 +14,8 @@ z_flat = np.ravel(z)
 X = reg.CreateDesignMatrix_X(x,y,n=5)
 
 def test_MSE_wo_resamp_OLS():
+    ''' Function testing the OLS code without resampling by comparing to scikit-learn. '''
+
     beta = reg.OLS_fit(X,z_flat)
     z_tilde = reg.OLS_predict(beta, X)
     MSE_own = mean_squared_error(z_flat,z_tilde)
@@ -25,10 +27,12 @@ def test_MSE_wo_resamp_OLS():
 
     tol = 1E-14
     success = abs(MSE_own - MSE_skl) < tol
-    msg = "Error: The MSE for own OLS code did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE for own OLS code did not match the MSE from scikit-learn.'
     assert success, msg
 
 def test_MSE_wo_resamp_Ridge():
+    ''' Function testing the Ridge code without resampling by comparing to scikit-learn. '''
+
     beta = reg.Ridge_fit(X,z_flat,0.1)
     z_tilde = reg.Ridge_predict(beta, X)
     MSE_own = mean_squared_error(z_flat,z_tilde)
@@ -40,10 +44,12 @@ def test_MSE_wo_resamp_Ridge():
 
     tol = 1E-14
     success = abs(MSE_own - MSE_skl) < tol
-    msg = "Error: The MSE for own Ridge code did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE for own Ridge code did not match the MSE from scikit-learn.'
     assert success, msg
 
 def test_MSE_wo_resamp_Lasso():
+    ''' Function testing the Lasso code without resampling by comparing to scikit-learn. '''
+
     beta = reg.Lasso_fit(X,z_flat,0.1)
     z_tilde = reg.Lasso_predict(beta, X)
     MSE_own = mean_squared_error(z_flat,z_tilde)
@@ -55,10 +61,12 @@ def test_MSE_wo_resamp_Lasso():
 
     tol = 1E-14
     success = abs(MSE_own - MSE_skl) < tol
-    msg = "Error: The MSE for own Lasso code did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE for own Lasso code did not match the MSE from scikit-learn.'
     assert success, msg
 
 def test_MSE_w_kfold_OLS():
+    ''' Function testing the k-fold code on OLS by comparing to scikit-learn. '''
+
     MSE_kf = reg.k_fold_cross_validation(
         X,z_flat, 5, reg.OLS_fit, reg.OLS_predict)[0]
 
@@ -75,10 +83,12 @@ def test_MSE_w_kfold_OLS():
 
     tol = 1E-3
     success = abs(MSE_kf - MSE_kf_skl) < tol
-    msg = "Error: The MSE from own k-fold code for OLS did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE from own k-fold code for OLS did not match the MSE from scikit-learn.'
     assert success, msg
 
 def test_MSE_w_kfold_Ridge():
+    ''' Function testing the k-fold code on Ridge by comparing to scikit-learn. '''
+
     MSE_kf = reg.k_fold_cross_validation(
         X,z_flat, 5, reg.Ridge_fit, reg.Ridge_predict, 0.01)[0]
 
@@ -95,10 +105,12 @@ def test_MSE_w_kfold_Ridge():
 
     tol = 1E-2
     success = abs(MSE_kf - MSE_kf_skl) < tol
-    msg = "Error: The MSE from own k-fold code for Ridge did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE from own k-fold code for Ridge did not match the MSE from scikit-learn.'
     assert success, msg
 
 def test_MSE_w_kfold_Lasso():
+    ''' Function testing the k-fold code on Lasso by comparing to scikit-learn. '''
+
     MSE_kf = reg.k_fold_cross_validation(
         X,z_flat, 5, reg.Lasso_fit, reg.Lasso_predict, 0.01)[0]
 
@@ -115,5 +127,5 @@ def test_MSE_w_kfold_Lasso():
 
     tol = 1E-2
     success = abs(MSE_kf - MSE_kf_skl) < tol
-    msg = "Error: The MSE from own k-fold code for Lasso did not match the MSE from scikit-learn."
+    msg = 'Error: The MSE from own k-fold code for Lasso did not match the MSE from scikit-learn.'
     assert success, msg
